e8624992c81afedda48198f966989d83de1c1dea
Jost von Hardenberg <jost.hardenberg@polito.it>
2021-05-14 12:25:03 +0200

    added post example

--- examples/testpost.sh
+++ examples/testpost.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #set -ex
-. scripts/posttools.sh
+. ../scripts/posttools.sh
 
 cd $EXPDIR
-for fn in ees??????t?
+for fn in ees100a00t?h???
 do
     echo Postprocessing $fn
     post $fn tas 31/60
     post $fn sic 31/60
-    post $fn sit 31/60
+#    post $fn sit 31/60
     post $fn al 31/60
     post $fn pr 31/60
 done
